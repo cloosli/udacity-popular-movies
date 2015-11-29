98082ebb00e12951232046f340fe77a7b205c419
Christian Loosli <loosli.christian@gmail.com>
2015-11-29 23:48:50 +0100

    create bundle keys class with MOVIE key

---
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/BundleKeys.java
@@ -0,0 +1,10 @@
+package com.loosli.christian.popularmovieapp.android.app;
+
+/**
+ * Created by ChristianL on 29.11.15.
+ */
+public abstract class BundleKeys {
+
+    public static final String MOVIE = "movie";
+
+}
