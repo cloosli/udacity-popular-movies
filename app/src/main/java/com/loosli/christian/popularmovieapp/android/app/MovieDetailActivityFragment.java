e0e8a338bfc150a59a2e12243fa815d8e1424f47
Christian Loosli <loosli.christian@gmail.com>
2015-11-30 22:18:13 +0100

    cleanup detail fragment

--- app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MovieDetailActivityFragment.java
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MovieDetailActivityFragment.java
@@ -10,9 +10,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.loosli.christian.popularmovieapp.android.app.entity.Movie;
+import com.loosli.christian.popularmovieapp.android.app.util.Util;
 import com.squareup.picasso.Picasso;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -32,27 +34,48 @@ public class MovieDetailActivityFragment extends Fragment {
         int imageWidth = getActivity().getResources().getDimensionPixelSize(R.dimen.movie_thumb_width);
         int imageHeight = getActivity().getResources().getDimensionPixelSize(R.dimen.movie_thumb_height);
 
+        Movie movie = null;
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(BundleKeys.MOVIE)) {
-            Movie movie = intent.getParcelableExtra(BundleKeys.MOVIE);
-            getActivity().setTitle(movie.getTitle());
-            ((TextView) rootView.findViewById(R.id.movie_detail_titel)).setText(movie.getTitle());
-            ((TextView) rootView.findViewById(R.id.movie_detail_description)).setText(movie.getOverview());
-            String voteAverage = String.format(getString(R.string.md_vote_average), movie.getRating());
-            ((TextView) rootView.findViewById(R.id.movie_detail_vote_average)).setText(voteAverage);
-            DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getActivity());
-            ((TextView) rootView.findViewById(R.id.movie_detail_release_date)).setText(getString(R.string.released) +
-                    dateFormat.format(movie.getReleaseDate()));
-
-            Picasso.with(getActivity())
-                    .load("http://image.tmdb.org/t/p/w185/" + movie.getPosterPath())
-                    .resize(imageWidth, imageHeight)
-                    .into((ImageView) rootView.findViewById(R.id.movie_detail_poster));
-
-            Picasso.with(getActivity())
-                    .load("http://image.tmdb.org/t/p/w185/" + movie.getBackdropPath())
-                    .into((ImageView) rootView.findViewById(R.id.backdrop));
+            movie = intent.getParcelableExtra(BundleKeys.MOVIE);
         }
+        if (movie == null) {
+            throw new IllegalStateException("no given movie!");
+        }
+
+        Picasso picasso = Picasso.with(getActivity());
+        ImageView poster = (ImageView) rootView.findViewById(R.id.poster);
+        ImageView backdrop = (ImageView) rootView.findViewById(R.id.backdrop);
+
+        int posterWidth = getResources().getDimensionPixelSize(R.dimen.details_poster_width);
+        int posterHeight = getResources().getDimensionPixelSize(R.dimen.details_poster_height);
+        picasso.load(Util.buildPosterUrl(movie.getPosterPath(), posterWidth))
+                .resize(posterWidth, posterHeight)
+                .centerCrop()
+                .into(poster);
+
+        int backdropWidth = Util.getScreenWidth(getActivity());
+        int backdropHeight = getResources().getDimensionPixelSize(R.dimen.details_backdrop_height);
+        picasso.load(Util.buildBackdropUrl(movie.getBackdropPath(), backdropWidth))
+                .resize(backdropWidth, backdropHeight)
+                .centerCrop()
+                .into(backdrop);
+
+        getActivity().setTitle(movie.getTitle());
+
+        ((TextView) rootView.findViewById(R.id.title)).setText(movie.getTitle());
+        ((TextView) rootView.findViewById(R.id.synopsis)).setText(movie.getOverview());
+
+        String voteAverage = String.format("%1$2.1f", movie.getRating());
+
+        ((TextView) rootView.findViewById(R.id.rating)).setText(voteAverage);
+        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getActivity());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(movie.getReleaseDate());
+
+        ((TextView) rootView.findViewById(R.id.release_date)).setText(Integer.toString(calendar.get(Calendar.YEAR)));
+                //getString(R.string.released) + dateFormat.format(movie.getReleaseDate()));
+
         return rootView;
     }
 }
---
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/util/Util.java
@@ -0,0 +1,122 @@
+package com.loosli.christian.popularmovieapp.android.app.util;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+import com.loosli.christian.popularmovieapp.android.app.BuildConfig;
+
+/**
+ * Created by ChristianL on 30.11.15.
+ */
+public class Util {
+
+    private static final Uri TMDB_IMAGE_BASE_URI = Uri.parse("http://image.tmdb.org/t/p/");
+
+    private interface TMDbImageWidth {
+        String getWidthString();
+
+        int getMaxWidth();
+    }
+
+    public enum TMDbPosterWidth implements TMDbImageWidth {
+        W92(92), W154(154), W185(185), W342(342), W500(500), W780(780), ORIGINAL(Integer.MAX_VALUE);
+
+        public final int maxWidth;
+
+        TMDbPosterWidth(int maxWidth) {
+            this.maxWidth = maxWidth;
+        }
+
+        public int getMaxWidth() {
+            return this.maxWidth;
+        }
+
+        public String getWidthString() {
+            return (this == ORIGINAL) ? "original" : "w" + this.maxWidth;
+        }
+    }
+
+    public enum TMDbBackdropWidth implements TMDbImageWidth {
+        W300(300), W780(780), W1280(1280), ORIGINAL(Integer.MAX_VALUE);
+
+        public final int maxWidth;
+
+        TMDbBackdropWidth(int maxWidth) {
+            this.maxWidth = maxWidth;
+        }
+
+        public int getMaxWidth() {
+            return this.maxWidth;
+        }
+
+        public String getWidthString() {
+            return (this == ORIGINAL) ? "original" : "w" + this.maxWidth;
+        }
+    }
+
+    public static String buildPosterUrl(String posterPath, int posterWidth) {
+        return buildImageUrl(posterPath, computeNextLowestPosterWidth(posterWidth));
+    }
+
+    public static String buildPosterUrl(String posterPath, TMDbPosterWidth tmdbPosterWidth) {
+        return buildImageUrl(posterPath, tmdbPosterWidth);
+    }
+
+    public static String buildBackdropUrl(String backdropPath, int backdropWidth) {
+        return buildImageUrl(backdropPath, computeNextLowestBackdropWidth(backdropWidth));
+    }
+
+    public static String buildBackdropUrl(String backdropPath, TMDbBackdropWidth tmdbBackdropWidth) {
+        return buildImageUrl(backdropPath, tmdbBackdropWidth);
+    }
+
+    private static <T extends TMDbImageWidth> String buildImageUrl(String imagePath, T tmdbImageWidth) {
+        if (BuildConfig.DEBUG) {
+            Log.d("Picasso", "Loading image of width " + tmdbImageWidth.getMaxWidth() + "px");
+        }
+        String relativePath = tmdbImageWidth.getWidthString() + "/" + imagePath;
+        return Uri.withAppendedPath(TMDB_IMAGE_BASE_URI, relativePath).toString();
+    }
+
+    public static int getScreenWidth(@NonNull Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
+    public static int multiplyColor(int srcColor, float factor) {
+        int alpha = Color.alpha(srcColor);
+        int red = (int) (Color.red(srcColor) * factor);
+        int green = (int) (Color.green(srcColor) * factor);
+        int blue = (int) (Color.blue(srcColor) * factor);
+        return Color.argb(alpha, red, green, blue);
+    }
+
+
+    // private methods
+
+    // 50 => W92, 92 => W92, 93 => W185, 999 => ORIGINAL
+    private static TMDbPosterWidth computeNextLowestPosterWidth(int posterWidth) {
+        for (TMDbPosterWidth enumWidth : TMDbPosterWidth.values()) {
+            if (0.8 * posterWidth <= enumWidth.maxWidth) {
+                return enumWidth;
+            }
+        }
+        return TMDbPosterWidth.ORIGINAL;
+    }
+
+    private static TMDbBackdropWidth computeNextLowestBackdropWidth(int backdropWidth) {
+        for (TMDbBackdropWidth enumWidth : TMDbBackdropWidth.values()) {
+            if (0.8 * backdropWidth <= enumWidth.maxWidth) {
+                return enumWidth;
+            }
+        }
+        return TMDbBackdropWidth.ORIGINAL;
+    }
+}
