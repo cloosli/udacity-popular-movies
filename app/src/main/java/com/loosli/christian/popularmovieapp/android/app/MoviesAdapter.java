ae09dc0e4d622e5ecdf80da08b9a8a68981719c9
Christian Loosli <loosli.christian@gmail.com>
2015-11-30 22:38:00 +0100

    build poster url based on the screen width

--- app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MoviesAdapter.java
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MoviesAdapter.java
@@ -7,6 +7,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import com.loosli.christian.popularmovieapp.android.app.entity.Movie;
+import com.loosli.christian.popularmovieapp.android.app.util.Util;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -21,17 +22,14 @@ public class MoviesAdapter extends BaseAdapter {
     private final Activity mContext;
     private final List<Movie> mMovies;
     //private final int mHeight;
-    //private final int mWidth;
-    private String mBasePosterUrl;
+    private final int mWidth;
 
     public MoviesAdapter(Activity context) {
         mContext = context;
         mMovies = new ArrayList<Movie>();
         //mHeight = Math.round(mContext.getResources().getDimension(R.dimen.poster_height));
-        //mWidth = Math.round(mContext.getResources().getDimension(R.dimen.poster_width));
+        mWidth = Math.round(Util.getScreenWidth(mContext)/2);
         //TODO: check connectivity speed
-        int posterSize = 185; //342;//(int) mContext.getResources().getDimension(R.dimen.poster_size);
-        mBasePosterUrl = "http://image.tmdb.org/t/p/w" + posterSize + "/";
     }
 
     public void addAll(Collection<Movie> xs) {
@@ -76,9 +74,9 @@ public class MoviesAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-        //Uri posterUri = Uri.parse(movie).buildUpon().build();
+
         Picasso.with(mContext)
-                .load(mBasePosterUrl + movie.getPosterPath())
+                .load(Util.buildPosterUrl(movie.getPosterPath(), mWidth))
                 .placeholder(R.drawable.empty_photo)
                 .into(imageView);
 
