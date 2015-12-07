a70fab1ce3f11f0e52eaaecd7191e894056bff75
Christian Loosli <loosli.christian@gmail.com>
2015-12-07 22:48:11 +0100

    updated javadoc

--- app/src/main/java/com/loosli/christian/popularmovieapp/android/app/util/Util.java
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/util/Util.java
@@ -13,6 +13,9 @@ import com.loosli.christian.popularmovieapp.android.app.BuildConfig;
 
 /**
  * Created by ChristianL on 30.11.15.
+ * <p/>
+ * Some parts are from vickychijwani,
+ * repo https://github.com/vickychijwani/udacity-p1-p2-popular-movies/blob/master/app/src/main/java/me/vickychijwani/popularmovies/util/Util.java
  */
 public class Util {
 
@@ -102,11 +105,11 @@ public class Util {
     /**
      * This method converts dp unit to equivalent pixels, depending on device density.
      *
-     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @param dp      A value in dp (density independent pixels) unit. Which we need to convert into pixels
      * @param context Context to get resources and device specific display metrics
      * @return A float value to represent px equivalent to dp depending on device density
      */
-    public static float convertDpToPixel(float dp, Context context){
+    public static float convertDpToPixel(float dp, Context context) {
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
         float px = dp * (metrics.densityDpi / 160f);
@@ -116,11 +119,11 @@ public class Util {
     /**
      * This method converts device specific pixels to density independent pixels.
      *
-     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @param px      A value in px (pixels) unit. Which we need to convert into db
      * @param context Context to get resources and device specific display metrics
      * @return A float value to represent dp equivalent to px value
      */
-    public static float convertPixelsToDp(float px, Context context){
+    public static float convertPixelsToDp(float px, Context context) {
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
         float dp = px / (metrics.densityDpi / 160f);
