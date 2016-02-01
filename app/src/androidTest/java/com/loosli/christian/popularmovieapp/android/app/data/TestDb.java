d3dbd39aab32829e28e911c2c16a5a1decc11852
Christian Loosli <loosli.christian@gmail.com>
2016-02-01 12:00:07 +0100

    test movie table with dummy data

--- app/src/androidTest/java/com/loosli/christian/popularmovieapp/android/app/data/TestDb.java
+++ app/src/androidTest/java/com/loosli/christian/popularmovieapp/android/app/data/TestDb.java
@@ -1,5 +1,6 @@
 package com.loosli.christian.popularmovieapp.android.app.data;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -76,6 +77,42 @@ public class TestDb extends AndroidTestCase {
     }
 
     public void testMovieTable() {
-        //TODO:
+        // First step: Get reference to writable database
+        // If there's an error in those massive SQL table creation Strings,
+        // errors will be thrown here when you try to get a writable database.
+        MovieDbHelper dbHelper = new MovieDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Second Step (Movie): Create movie values
+        ContentValues movieValues = TestUtilities.createMovieValues();
+
+        // Third Step (Weather): Insert ContentValues into database and get a row ID back
+        long weatherRowId = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, movieValues);
+        assertTrue(weatherRowId != -1);
+
+        // Fourth Step: Query the database and receive a Cursor back
+        // A cursor is your primary interface to the query results.
+        Cursor movieCursor = db.query(
+                MovieContract.MovieEntry.TABLE_NAME,  // Table to Query
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null  // sort order
+        );
+
+        // Move the cursor to the first valid database row and check to see if we have any rows
+        assertTrue("Error: No Records returned from location query", movieCursor.moveToFirst());
+
+        // Fifth Step: Validate the movie Query
+        TestUtilities.validateCurrentRecord("testInsertReadDb movieEntry failed to validate", movieCursor, movieValues);
+
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse("Error: More than one record returned from weather query", movieCursor.moveToNext());
+
+        // Sixth Step: Close cursor and database
+        movieCursor.close();
+        dbHelper.close();
     }
 }
--- app/src/androidTest/java/com/loosli/christian/popularmovieapp/android/app/data/TestUtilities.java
+++ app/src/androidTest/java/com/loosli/christian/popularmovieapp/android/app/data/TestUtilities.java
@@ -1,9 +1,41 @@
 package com.loosli.christian.popularmovieapp.android.app.data;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.test.AndroidTestCase;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by ChristianL on 01.02.16.
  */
 public class TestUtilities extends AndroidTestCase {
+    static final long TEST_DATE = 1419033600L;  // December 20th, 2014
+
+    public static ContentValues createMovieValues() {
+        ContentValues movieValues = new ContentValues();
+        movieValues.put(MovieContract.MovieEntry._ID, "123");
+        movieValues.put(MovieContract.MovieEntry.COLUMN_TITLE, "Inside Out");
+        movieValues.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, "Growing up can be a bumpy road, and it's no exception for Riley,...");
+        movieValues.put(MovieContract.MovieEntry.COLUMN_POSTERPATH, "/aAmfIX3TT40zUHGcCKrlOZRKC7u.jpg");
+        movieValues.put(MovieContract.MovieEntry.COLUMN_BACKDROPPATH, "/szytSpLAyBh3ULei3x663mAv5ZT.jpg");
+        movieValues.put(MovieContract.MovieEntry.COLUMN_RATING, 8.04);
+        movieValues.put(MovieContract.MovieEntry.COLUMN_RELEASEDATE, TEST_DATE);
+
+        return movieValues;
+    }
+
+    public static void validateCurrentRecord(String error, Cursor valueCursor, ContentValues expectedValues) {
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse("Column '" + columnName + "' not found. " + error, idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals("Value '" + entry.getValue().toString() +
+                    "' did not match the expected value '" +
+                    expectedValue + "'. " + error, expectedValue, valueCursor.getString(idx));
+        }
+    }
 }
