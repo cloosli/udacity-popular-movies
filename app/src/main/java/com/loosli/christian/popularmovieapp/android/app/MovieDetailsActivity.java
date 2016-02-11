7e5d7b157287d5ddd27133be9e87d1f346957eaf
Christian Loosli <loosli.christian@gmail.com>
2016-02-11 21:43:12 +0100

    fix the Up button in the action bar

--- app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MovieDetailsActivity.java
+++ app/src/main/java/com/loosli/christian/popularmovieapp/android/app/MovieDetailsActivity.java
@@ -1,12 +1,22 @@
 package com.loosli.christian.popularmovieapp.android.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.loosli.christian.popularmovieapp.android.app.entity.Movie;
 
+/**
+ * An activity representing a single Movie detail screen. This
+ * activity is only used narrow width devices. On tablet-size devices,
+ * item details are presented side-by-side with a list of items
+ * in a {@link MoviesActivity}.
+ */
 public class MovieDetailsActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +27,21 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+        //
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
@@ -33,5 +58,22 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpTo(this, new Intent(this, MoviesActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
